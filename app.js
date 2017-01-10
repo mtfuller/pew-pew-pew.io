04c40cdf484a23c47b4d0b63e26db63d16bc1941
Thomas Fuller <Thomas Fuller>
2017-01-09 22:03:57 -0500

    improved connection between server/client and user input

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var app = require('express')();
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var uuid = require('node-uuid');
-var hello = require('./game_core');
+var io = require('socket.io')(server, {'pingInterval': 2000, 'pingTimeout': 5000});
+var uuid = require('uuid');
+var pew_game_engine = require('./game_core');
 
 var number = 1;
 var entities = {};
@@ -14,59 +14,60 @@ app.get('/', function(req, res) {
 
 // Wait for incomming socket to connect
 io.on('connection', function (socket) {
-
-
+    var userIsValid = true;
     var user_id = uuid.v1();
-    console.log(socket.id);
+    var conn_time = new Date().getTime();
 
     // Response that user has been connected
-    socket.emit('connected', { id: user_id });
+    socket.emit('connected', {});
 
-    // Check if new player can be created
-    if (true) {
-        // Validate User
-        //socket.emit('authenticated', { id: number });
-        //number++;
+    console.log(Object.keys(io.sockets.sockets));
 
-        // Add player to game
+    socket.on('register', function (data) {
+      if (new Date().getTime() - conn_time <= 5000) return;
 
+      // Validate User
+      if (userIsValid) {
+          // Add player to game
+          pew_game_engine.addEntity(user_id);
 
-        // Wait for input from player
-        socket.on('input', function (data) {
-            if (entities.hasOwnProperty(data.id)) {
-                entities[data.id].x = data.x;
-                entities[data.id].y = data.y;
-            } else {
-                entities[data.id] = {
-                    x: data.x,
-                    y: data.y
-                }
-            }
-        });
+          // Tell user that they can begin to play
+          socket.emit('authenticated', { id: user_id });
 
-        // When player asks for update
-        socket.on('update', function () {
-            socket.emit('circle', entities);
-            socket.emit('render', hello.getClientData());
-        });
-    } else {
+          // Wait for input from player
+          socket.on('input', function (data) {
+              if (entities.hasOwnProperty(data.id)) {
+                  entities[data.id].x = data.x;
+                  entities[data.id].y = data.y;
+              } else {
+                  entities[data.id] = {
+                      x: data.x,
+                      y: data.y
+                  }
+              }
+          });
 
-    };
+          // When player asks for update
+          socket.on('update', function () {
+              socket.emit('game_data', pew_game_engine.getClientData());
+          });
+        } else {
+          socket.disconnect();
+        }
+    });
 
     // When User disconnects
     socket.on('disconnect', function () {
         socket.emit('disconnected');
-        delete entities[user_id];
-        console.log(entities);
+        pew_game_engine.removeEntity(user_id);
+        console.log("The user ("+user_id+") has disconnected.");
     });
-
 });
 
 server.listen(3000, function(){
-  console.log('SERVER START - Serving on port 3000')
+  console.log('SERVER START - Serving on port 3000');
   // Server START
   // Setup Game
-  hello.setupGame();
-  hello.addEntity();
-  hello.startGame();
+  pew_game_engine.setupGame();
+  pew_game_engine.startGame();
 });
--- game_core/index.js
+++ game_core/index.js
@@ -7,7 +7,6 @@
 // Systems
 
 var entity = require('./entities/entity.js');
-var tester;
 
 var positionComp = require('./components/position-component.js');
 
@@ -17,20 +16,24 @@ var velocitySystem = require('./systems/velocity-system.js');
 var player = require('./entities/player.js');
 
 // GameManager
-
 var return_obj = {
     entities: {},
     systems: {},
     setupGame: function() {
+        console.log('Game is setup!')
         this.addSystem("Position", positionSystem);
         this.addSystem("Velocity", velocitySystem);
     },
-    addEntity: function() {
-        var entityId = Math.random().toString(36).substring(12);
-        tester = entityId;
+    addEntity: function(entityId) {
+        //var entityId = Math.random().toString(36).substring(12);
         this.entities[entityId] = new player.Player(entityId, 1, 100, 100, 20, 10);
         this.subscribeEntityToSystem(entityId, "Position");
         this.subscribeEntityToSystem(entityId, "Velocity");
+    },
+    removeEntity: function(entityId) {
+      delete this.entities[entityId];
+      this.unsubscribeEntityToSystem(entityId, "Position");
+      this.unsubscribeEntityToSystem(entityId, "Velocity");
     },
     addSystem: function(name, system) {
         if (name != null && !this.systems.hasOwnProperty(name)) {
@@ -43,6 +46,9 @@ var return_obj = {
     registerPlayer: function() {},
     subscribeEntityToSystem: function(entityId, systemName) {
         this.systems[systemName].addEntity(entityId);
+    },
+    unsubscribeEntityToSystem: function(entityId, systemName) {
+        this.systems[systemName].removeEntity(entityId);
     },
     updateGame: function() {
         for (system in this.systems) {
